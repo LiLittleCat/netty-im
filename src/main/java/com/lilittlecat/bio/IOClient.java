1cb4e82c1986c6eced1e110c1aa068b303542c8e
LiLittleCat <luoyukongchan@outlook.com>
2022-06-10 01:01:51 +0800

    add files.

---
+++ src/main/java/com/lilittlecat/bio/IOClient.java
@@ -0,0 +1,27 @@
+package com.lilittlecat.bio;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Date;
+
+/**
+ * @author LiLittleCat
+ * @since 2022/6/10
+ */
+public class IOClient {
+    public static void main(String[] args) {
+        new Thread(() -> {
+            try {
+                Socket socket = new Socket("127.0.0.1", 8000);
+                while (true) {
+                    try {
+                        socket.getOutputStream().write((new Date() + ": hello world").getBytes());
+                        Thread.sleep(2000);
+                    } catch (Exception e) {
+                    }
+                }
+            } catch (IOException e) {
+            }
+        }).start();
+    }
+}
